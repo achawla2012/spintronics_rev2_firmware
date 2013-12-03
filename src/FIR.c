7575a5739ea8c56d0644fdacfd9099f0d076d554
msandstedt <michael.sandstedt@gmail.com>
2013-12-03 15:09:04 -0600

    FIR is inline again

--- src/FIR.c
+++ src/FIR.c
@@ -14,7 +14,34 @@ static int16_t delayLine[FILTER_TAPS] __attribute__((space(xmemory), eds, aligne
 inline int16_t FIR(int16_t input)
 {
     static int16_t *dly_ptr = delayLine;
-    return asmFIR(input, &dly_ptr, coef_tbl);
+    volatile register int16_t *reg_dly_ptr asm("w9");
+    volatile register int16_t *reg_coef_ptr;
+    volatile register uint16_t en_modulo = 0x8009;
+    volatile register int16_t result;
+
+    reg_dly_ptr = dly_ptr;
+    reg_coef_ptr = coef_tbl;
+
+
+
+
+    volatile __asm__("MOV %2, MODCON\n"
+                     "MOV %3, [%4]\n"
+                     "CLR A, [%4]-=2, w4, [%5]+=2, w5\n"
+                     "DO #15, ACCUMULATE\n"
+                     "MAC w4*w5, A, [%4]-=2, w4, [%5]+=2, w5\n"
+                     "ACCUMULATE: MAC w4*w5, A, [%4]-=2, w4, [%5]+=2, w5\n"
+                     "MAC w4*w5, A, [%4]-=2, w4, [%5]+=2, w5\n"
+                     "MAC w4*w5, A, [%4], w4, [%5], w5\n"
+                     "MAC w4*w5, A\n"
+                     "CLR MODCON\n"
+                     "MOV ACCAH, %0"
+                     : "=r"(result), "+x"(reg_dly_ptr)
+                     : "r"(en_modulo), "r"(input), "+x"(reg_dly_ptr), "y"(reg_coef_ptr)
+                     : "w4", "w5", "ACCAL", "ACCAH", "ACCAU");
+
+    dly_ptr = reg_dly_ptr;
+    return result;
 }
 
 void firInit(void)
