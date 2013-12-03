b153fa4eebd741911acb0ddb494ddd9f82868686
msandstedt <michael.sandstedt@gmail.com>
2013-12-03 15:36:34 -0600

    final asm inline fix

--- src/FIR.c
+++ src/FIR.c
@@ -15,30 +15,27 @@ inline int16_t FIR(int16_t input)
 {
     static int16_t *dly_ptr = delayLine;
     volatile register int16_t *reg_dly_ptr asm("w9");
-    volatile register int16_t *reg_coef_ptr;
+    volatile register int16_t *reg_coef_ptr asm("w11");
     volatile register uint16_t en_modulo = 0x8009;
     volatile register int16_t result;
 
     reg_dly_ptr = dly_ptr;
-    reg_coef_ptr = coef_tbl;
+    reg_coef_ptr = (uint16_t *)coef_tbl;
 
-
-
-
-    volatile __asm__("MOV %2, MODCON\n"
-                     "MOV %3, [%4]\n"
-                     "CLR A, [%4]-=2, w4, [%5]+=2, w5\n"
-                     "DO #15, ACCUMULATE\n"
-                     "MAC w4*w5, A, [%4]-=2, w4, [%5]+=2, w5\n"
-                     "ACCUMULATE: MAC w4*w5, A, [%4]-=2, w4, [%5]+=2, w5\n"
-                     "MAC w4*w5, A, [%4]-=2, w4, [%5]+=2, w5\n"
-                     "MAC w4*w5, A, [%4], w4, [%5], w5\n"
-                     "MAC w4*w5, A\n"
-                     "CLR MODCON\n"
-                     "MOV ACCAH, %0"
-                     : "=r"(result), "+x"(reg_dly_ptr)
-                     : "r"(en_modulo), "r"(input), "+x"(reg_dly_ptr), "y"(reg_coef_ptr)
-                     : "w4", "w5", "ACCAL", "ACCAH", "ACCAU");
+    __asm__ volatile ("MOV %2, MODCON\n"
+                      "MOV %3, [%1]\n"
+                      "CLR A, [%1]-=2, w4, [%4]+=2, w5\n"
+                      "DO #15, ACCUMULATE\n"
+                      "MAC w4*w5, A, [%1]-=2, w4, [%4]+=2, w5\n"
+                      "ACCUMULATE: MAC w4*w5, A, [%1]-=2, w4, [%4]+=2, w5\n"
+                      "MAC w4*w5, A, [%1]-=2, w4, [%4]+=2, w5\n"
+                      "MAC w4*w5, A, [%1], w4, [%4], w5\n"
+                      "MAC w4*w5, A\n"
+                      "CLR MODCON\n"
+                      "MOV ACCAH, %0"
+                      : "=r"(result), "+x"(reg_dly_ptr)
+                      : "r"(en_modulo), "r"(input), "y"(reg_coef_ptr)
+                      : "w4", "w5");
 
     dly_ptr = reg_dly_ptr;
     return result;
